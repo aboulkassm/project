43a3b470c3c8790bf6f5191885994c25d964fdc6
aboulkassm <123119017+aboulkassm@users.noreply.github.com>
2023-06-06 20:02:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-(function () {
-    [...document.querySelectorAll('.control:not(#theme-btn)')].forEach(button => {
-        button.addEventListener("click", function() {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-        })
+document.addEventListener("DOMContentLoaded", function() {
+    const controlButtons = [...document.querySelectorAll('.control:not(#theme-btn)')];
+    const sections = [...document.querySelectorAll('section')];
+  
+    controlButtons.forEach(button => {
+      button.addEventListener("click", function() {
+        controlButtons.forEach(btn => {
+          btn.classList.remove("active-btn");
+        });
+        this.classList.add("active-btn");
+  
+        sections.forEach(section => {
+          section.classList.remove("active");
+        });
+  
+        const targetSectionId = this.dataset.id;
+        const targetSection = document.getElementById(targetSectionId);
+        if (targetSection) {
+          targetSection.classList.add("active");
+        }
+      });
     });
+  
     document.querySelector("#theme-btn").addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
-    })
-})();
+      document.body.classList.toggle("light-mode");
+    });
+  });
+  
